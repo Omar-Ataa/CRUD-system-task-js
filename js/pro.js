91a94bb8f410580a1bd8a3ccff14f9e9443a176b
Omar Ataa <168627320+Omar-Ataa@users.noreply.github.com>
2024-11-16 04:03:27 +0200

    Update pro.js

--- js/pro.js
+++ js/pro.js
@@ -1,5 +1,3 @@
-
-
 var productName = document.getElementById('nameInput');
 var productType = document.getElementById('typeInput');
 var productPrice = document.getElementById('priceInput');
@@ -8,159 +6,137 @@ var tbody = document.getElementById('tbody');
 var productSearch = document.getElementById('searchInput');
 var addBtn = document.getElementById('addBtn');
 
+clearForm();
 
-    clearForm();
+// Retrieve data from localStorage if available
+var productList = JSON.parse(localStorage.getItem('productData')) || [];
 
-if( localStorage.getItem('productData') == null ){
-    var productList = [] ;
-}
-else{
-    var productList = JSON.parse(localStorage.getItem('productData'));
-
-    showData()
-}
-
-// clear-------------------------
-function clearForm(){
-    
-    productName.value="";
-    productType.value='';
-    productPrice.value='';
-    productDescription.value='';
-    
-    addBtn.innerHTML = 'add product';
-}
-// add--------------------------
-function addProduct(){
-
-    if(addBtn.innerHTML.includes('add') ){
-    var product = { 
-        namee : productName.value ,
-        type : productType.value ,
-        price : productPrice.value ,
-        description : productDescription.value ,
-    };
-
-  
-    productList.push(product);
-    
-    var stringData = JSON.stringify(productList);
-    localStorage.setItem('productData' , stringData);
-
-
-}   else{
-    updateProduct();
-}
+// Display data if available
+if (productList.length > 0) {
     showData();
 }
 
+// Clear form fields-------------------------
+function clearForm() {
+    productName.value = "";
+    productType.value = "";
+    productPrice.value = "";
+    productDescription.value = "";
 
-// show data---------------------
-function showData(){
+    addBtn.innerHTML = 'Add Product';
+}
 
-    var trs = '' ;
-    for ( var i = 0 ; i < productList.length ; i++ ){
-
-     trs += ` <tr>
-                    <td scope="row">${i}</td>
-                    <td>${productList[i].namee}</td>
-                    <td>${productList[i].type}</td>
-                    <td>${productList[i].price}</td>
-                    <td>${productList[i].description}</td>
-
-                    <td>
-                    <button onclick="deleteProduct(${i})" 
-                    class=" btn btn-outline-danger ">
-                     <i  class="fa-solid fa-trash-can text-danger "></i> 
-                     </button> 
-                     </td>
-
-                    <td>
-                     <button onclick="showDataOnForm(${i})"
-                     class=" btn btn-outline-warning ">
-                     <i class="fa-regular fa-pen-to-square text-warning "></i> 
-                     </button>
-                      </td>
-             </tr> `
-
-    
+// Add Product--------------------------
+function addProduct() {
+    // Check if all fields are filled
+    if (!productName.value.trim() || !productType.value.trim() || !productPrice.value.trim() || !productDescription.value.trim()) {
+        alert("Please fill in all fields!");
+        return; // Stop execution if any field is empty
     }
 
-    tbody.innerHTML= trs ;
-}
+    if (addBtn.innerHTML.includes('Add')) {
+        var product = {
+            namee: productName.value,
+            type: productType.value,
+            price: productPrice.value,
+            description: productDescription.value
+        };
 
+        productList.push(product);
+        localStorage.setItem('productData', JSON.stringify(productList));
+    } else {
+        updateProduct();
+    }
 
-// Delete-------------------
-function deleteProduct(index){
-    
-    productList.splice(index,1);
-
-    localStorage.setItem('productData' , JSON.stringify(productList) ) ;
     showData();
 }
 
-// search-------------
-function searchFun(){
-
-            console.log(productSearch.value);
-            var trs = '' ;
-              for ( var i = 0 ; i < productList.length ; i++){
-
-                if(productList[i].namee.toLowerCase().includes(productSearch.value.toLowerCase()) == true){
-                    
-                 trs += ` <tr>
+// Show data---------------------
+function showData() {
+    var trs = '';
+    for (var i = 0; i < productList.length; i++) {
+        trs += `<tr>
                     <td scope="row">${i}</td>
                     <td>${productList[i].namee}</td>
                     <td>${productList[i].type}</td>
                     <td>${productList[i].price}</td>
                     <td>${productList[i].description}</td>
-
                     <td>
-                    <button onclick="deleteProduct(${i})" 
-                    class=" btn btn-outline-danger ">
-                     <i  class="fa-solid fa-trash-can text-danger "></i> 
-                     </button> 
-                     </td>
-
+                        <button onclick="deleteProduct(${i})" class="btn btn-outline-danger">
+                            <i class="fa-solid fa-trash-can text-danger"></i>
+                        </button>
+                    </td>
                     <td>
-                     <button 
-                     class=" btn btn-outline-warning ">
-                     <i class="fa-regular fa-pen-to-square text-warning "></i> 
-                     </button>
-                      </td>
-                 </tr> `
-
-                }
-                  }
-
-                    tbody.innerHTML= trs ;   
+                        <button onclick="showDataOnForm(${i})" class="btn btn-outline-warning">
+                            <i class="fa-regular fa-pen-to-square text-warning"></i>
+                        </button>
+                    </td>
+                </tr>`;
+    }
+    tbody.innerHTML = trs;
 }
 
-// show Data On Form--------------
+// Delete Product-------------------
+function deleteProduct(index) {
+    productList.splice(index, 1);
+    localStorage.setItem('productData', JSON.stringify(productList));
+    showData();
+}
+
+// Search-------------------
+function searchFun() {
+    if (!productSearch.value.trim()) {
+        alert("Please enter a search term.");
+        return;
+    }
+
+    var trs = '';
+    for (var i = 0; i < productList.length; i++) {
+        if (productList[i].namee.toLowerCase().includes(productSearch.value.toLowerCase())) {
+            trs += `<tr>
+                        <td scope="row">${i}</td>
+                        <td>${productList[i].namee}</td>
+                        <td>${productList[i].type}</td>
+                        <td>${productList[i].price}</td>
+                        <td>${productList[i].description}</td>
+                        <td>
+                            <button onclick="deleteProduct(${i})" class="btn btn-outline-danger">
+                                <i class="fa-solid fa-trash-can text-danger"></i>
+                            </button>
+                        </td>
+                        <td>
+                            <button onclick="showDataOnForm(${i})" class="btn btn-outline-warning">
+                                <i class="fa-regular fa-pen-to-square text-warning"></i>
+                            </button>
+                        </td>
+                    </tr>`;
+        }
+    }
+    tbody.innerHTML = trs;
+}
+
+// Show data on form----------------
 var globalIndex = 0;
-productName.value = null;
-function showDataOnForm(index){
+
+function showDataOnForm(index) {
     globalIndex = index;
     productName.value = productList[index].namee;
     productType.value = productList[index].type;
     productPrice.value = productList[index].price;
     productDescription.value = productList[index].description;
 
-    addBtn.innerHTML= 'update';
+    addBtn.innerHTML = 'Update';
 }
 
-// Update----------------
-function updateProduct(){
-
+// Update Product----------------
+function updateProduct() {
     productList[globalIndex].namee = productName.value;
     productList[globalIndex].type = productType.value;
     productList[globalIndex].price = productPrice.value;
-    productList[globalIndex].description = productDescription.value ;
+    productList[globalIndex].description = productDescription.value;
 
-    var stringData = JSON.stringify(productList);
-    localStorage.setItem('productData' , stringData);
+    localStorage.setItem('productData', JSON.stringify(productList));
 
-   clearForm();
-
-   addBtn.innerHTML='add product';
+    clearForm();
+    addBtn.innerHTML = 'Add Product';
 }
